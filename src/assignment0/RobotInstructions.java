b36aa76ea8cb9a6cbacc80edfc54a2898ead26d3
Atleysherman <a.a.sherman@wustl.edu>
2026-01-21 12:48:18 -0600

    basic flower

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,25 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(60);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(120);
+		robot.turnLeft(90);
+		robot.forward(120);
+		robot.turnLeft(90);
+		robot.forward(120);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(120);
+		robot.turnRight(180);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(180);
+		robot.forward(120);
 	}
 
 }
